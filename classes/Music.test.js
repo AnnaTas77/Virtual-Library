ef8f3fd18d181379a29e58db4ebc16a443b38afd
Daniella Horvath <daniella.horvath@cx3yw2l2fy.home>
2024-07-23 16:41:49 +0100

    Completed Music section

--- classes/Music.test.js
+++ classes/Music.test.js
@@ -17,7 +17,7 @@ describe('Music tests', () => {
   test('Music summary method returns the correct summary', () => {
     const music2 = new Music('Renaissance', 2022, 'R&B', 'Beyonce', 3734)
     expect(music2.summary()).toBe(
-      'Title: Renaissance, Artist: Beyonce, Year: 2022, Genre: R&B, Length: 3734 seconds'
+      'Title: Renaissance, Artist: Beyonce, Year: 2022, Genre: R&B, Length: 3734'
     )
   })
 
